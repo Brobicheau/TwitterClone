04ea49122116dfe18ca197f0d8d72525c02d0845
root <root@twitter-front.cloud.compas.cs.stonybrook.edu>
2017-03-20 20:17:39 +0000

    Skeleton Twitter commit

---
+++ twitter.js
@@ -0,0 +1,347 @@
+/****************************************************************
+*	Notes: 
+*		- ALL POST PARAMETERS ARE JSON (application/json)
+*		- ALL RETURN TYPES MUST BE JSON AS WELL
+*
+*****************************************************************/
+
+
+
+var express = require('express'); // EXPRESS MODULE
+var parser = require('body-parser');//forparsing req params, will change to multer
+var app = express();
+
+
+
+
+//MAIN PAGE OF TWITTER
+app.post('/', function(req, res) {
+	console.log("GET REQUEST SUCCESSFUL");
+	res.send();
+});
+
+
+
+/********************************************
+*	/adduser description - POST: 
+*	This request will register a new user account IF 
+*	the username AND email are unique. It must then send
+* 	email with verification key (Use nodes email-verification module)
+*
+*	Request Parameters
+*		- username = req.body.username
+*		- email = req.body.email
+*		- password = req.body.password
+*
+*	Return:
+*	-	status: "OK" or "error"
+*	- error: error message(if error)
+*
+************************************************/		
+app.post('/adduser', function(req,res){
+
+
+});
+
+
+/********************************************
+*	/login description - POST: 
+*	This post request will log the user into the site IF its 
+*	username and password are found in the database. It will then set
+*	the users cookie to enable the session. 
+*
+*	Request Parameters
+*		- username = req.body.username
+*		- password = req.body.password
+*
+*	Return:
+*	-	status: "OK" or "error"
+*	- error: error message(if error)
+*
+************************************************/
+app.post('/login', function(req,res){
+
+
+});
+
+
+/********************************************
+*	/logout description - POST: 
+*	This will log the user out by setting its session cookie to null
+*
+*	Request Parameters
+*		- NONE
+*
+*	Return:
+*	-	status: "OK" or "error"
+*	- error: error message(if error)
+*
+************************************************/
+app.post('/logout', function(req,res){
+
+
+});
+
+
+/********************************************
+*	/verify description - POST: 
+*	This will verify users account using its email and a backdoor key
+*	Users account cannot be used until it is verified.
+*
+*	Request Parameters
+*		- email = req.body.email
+		- key = req.body.key = "abracadabra" (backdoor)
+*
+*	Return:
+*	-	status: "OK" or "error"
+*	- error: error message(if error)
+*
+************************************************/
+app.post('/verify', function(req,res){
+
+
+});
+
+
+/********************************************
+*	/additem description - POST: 
+*	This will post a new tweet only if a user is found in session
+*
+*	Request Parameters
+*		- content = req.body.content
+*		- parent(STAGE 3) = req.body.parent = 
+*
+*	Return:
+*	-	status: "OK" or "error"
+	- id: uniqur tweet IF (if OK)
+*	- error: error message(if error)
+*
+************************************************/
+app.post('/additem', function(req,res){
+
+
+});
+
+
+/********************************************
+*	/item/<id> description - GET: 
+*	Searches and returns for tweet of given ID
+*
+*	Request Parameters
+*		- NONE
+*
+*	Return:
+*	-	status: "OK" or "error"
+*	- item: {
+*		- id : tweet id
+*		- username: username who sent tweet
+*		- content: message body of tweet
+*		- timestamp: timestamp, represeted as Unix time in seconds
+*	}
+*	- error: error message(if error)
+*	- media(STAGE 3): ID of asssociated media file(if any)
+*
+************************************************/
+app.get('/item/<id>', function(req,res){
+
+});
+
+/********************************************
+*	/item/<id> description - DELETE (STAGE 2): 
+*	Deletes tweet of given ID, also deleteds associated media
+*
+*	Request Parameters
+*		- NONE
+*
+*	Return:
+*	- success or failure
+*
+************************************************/
+app.delete('/item/<id>', function(req,res){
+
+
+});
+
+
+/********************************************
+*	/search description - POST : 
+*	Gets list of all tweets(Expand description later)
+*
+*	Request Parameters
+*		- timestamp: search tweets from this time and earlier
+*			- Represented as Unix time in seconds
+*			- Integer, optional
+*			- Default: Current time
+*		- limit: number of tweets to return
+*			- Integerr, option
+*			- Default: 25
+*			- Max: 100
+*		(STAGE 2 REQUEST PARAMS)
+*			- q: search query
+*			- username: username
+*				- String optional
+*				- Filter by username
+*			- following: only show tweets made by users that logged in user follows
+*				- Booklean, optional
+*				- Default: true
+*		(STAGE 3 PARAMS)
+*			- rank: Orfer returned tweets by 'time' or by "interest" (weighting itme vs number of likes and retweets)
+*				- String optional
+*				- Default: true
+*			- parent: Returns tweets made in reply to requested tweet
+*				- Boolean, optional
+*				- Default: false
+*	Return:
+*	- status: "OK" or "error"
+*	- items: Array of tweet objects (see/item/:id)
+*	- error: error message (if error)
+*
+************************************************/
+app.post('/search', function(req,res){
+
+
+});
+
+/********************************************
+*	/user/<username> description - GET (STAGE 2): 
+*	Gets the users profile information
+*
+*	Request Parameters
+*		- username: username to retrieve = req.body.username
+*
+*	Return:
+*	- status: "OK" or "error"
+*	- user: {
+*		- email: 
+*		- followers: follower count
+*		- following: following count
+*	}
+*
+************************************************/
+app.get('/user/<username>', function(req,res){
+
+
+});
+
+
+/******************************************************************
+*	/user/<username>/followers description - GET (STAGE 2): 
+*	Gets list of users following "username"
+*
+*	Request Parameters
+*		- limit: number of usernames to return = req.body.username
+*			- Integer, optional
+*			- Default: 50
+*			- Max: 200
+*
+*	Return:
+*	- status: "OK" or "error"
+*	- users: list of usernames (strings) 
+*
+*******************************************************************/
+app.get('/user/<username>/followers', function(req,res){
+
+
+});
+
+
+/******************************************************************
+*	/user/<username>/following description - GET (STAGE 2): 
+*	Gets list of users "username" is following
+*
+*	Request Parameters
+*		- limit: number of usernames to return = req.body.username
+*			- Integer, optional
+*			- Default: 50
+*			- Max: 200
+*
+*	Return:
+*	- status: "OK" or "error"
+*	- users: list of usernames (strings) 
+*
+*******************************************************************/
+app.post('/user/<username>/following', function(req,res){
+
+
+});
+
+
+/******************************************************************
+*	/follow description - POST (STAGE 2): 
+*	Followsor unfollows a user
+*
+*	Request Parameters
+*		- username: username to follow = req.body.username
+*		- follow: = req.body.follow
+*			- Boolean
+*			- Default: true
+*
+*	Return:
+*	- status: "OK" or "error"
+*
+*******************************************************************/
+app.post('/follow', function(req,res){
+
+
+});
+
+
+/******************************************************************
+*	/item/<id>/like description - POST (STAGE 3): 
+*	Likes or unlikes a tweet ID
+*
+*	Request Parameters
+*		- like: = req.body.like
+*			- Boolean
+*			- Default: true
+*
+*	Return:
+*	- status: "OK" or "error"
+*
+*******************************************************************/
+app.post('/item/<id>/like', function(req,res){
+
+
+});
+
+
+/******************************************************************
+*	/addmedia description - POST (STAGE 3): 
+*	adds media file (photo or video)
+*
+*	Request Parameters (type is multipart/form-data (gotta use multer module))
+*		- content: binary content of ile being uploaded = req.file.content
+*
+*	Return:
+*	- status: "OK" or "error"
+*	- id: ID of uploaded media
+*	- error: error message (if error)
+*
+*******************************************************************/
+app.post('/addmedia', function(req,res){
+
+
+});
+
+
+/******************************************************************
+*	/media/<id> description - GET (STAGE 3): 
+*	Gets media file by id
+*
+*	Request Parameters (type is multipart/form-data (gotta use multer module))
+*		- content: binary content of ile being uploaded = req.file.content
+*
+*	Return:
+*	- returns media file (image or video)
+*
+*******************************************************************/
+app.post('/media/<id>', function(req,res){
+
+
+});
+
+
+
+/* using reverse proxy, so listening on localhost port 300x*/
+app.listen(3000, 'localhost');
+console.log("listening on port 3000");
